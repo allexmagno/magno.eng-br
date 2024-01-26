3560d93ecfb61f83b72c08d32fbb0834c545ed1d
Magno <ct.allex@gmail.com>
2024-01-26 00:38:22 -0300

    mesclando arquivos

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706240070';
+const cacheName = 'chirpy-1706240278';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
